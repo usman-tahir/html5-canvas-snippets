bd6068dc887b18d5d7cf30190037857ff987f55e
usman-tahir <tahir.usman.ali94@outlook.com>
2016-10-16 16:32:50 -0400

    added debug statements and comments for clarification

--- hello-world-canvas/hello-world-canvas.js
+++ hello-world-canvas/hello-world-canvas.js
@@ -3,7 +3,8 @@
 (function () {
   "use strict";
   var Debugger = function () {};
-
+  
+  // Basic debugger
   Debugger.log = function (message) {
     try {
       console.log(message);
@@ -13,7 +14,7 @@
   };
   
   function canvasSupport() {
-    return Modernizr.canvas;
+    return Modernizr.canvas; // Determines if the user-agent is able to use html5 canvas
   }
   
   function canvasApp() {
@@ -28,18 +29,22 @@
     canvasOne = document.getElementById("canvas-one");
     context = canvasOne.getContext("2d");
     
-    Debugger.log("Drawing canvas on: #canvas-one");
+    Debugger.log("Drawing on canvas: #canvas-one");
     
     function drawScreen() {
       // Draw the background
-      context.fillStyle = "#ffffaa";
-      context.fillRect(0, 0, 500, 300);
+      context.fillStyle = "#ffffaa"; // Fill color for the background
+      context.fillRect(0, 0, 500, 300); // Dimensions for the rectangle (as well as starting coordinates)
+      
+      Debugger.log("Created rectangle background for: #canvas-one");
       
       // Generate the text
       context.fillStyle = "#000000";
       context.font = "20px Sans-Serif";
       context.textBaseline = "top";
-      context.fillText("Hello World!", 195, 80);
+      context.fillText("Hello World!", 195, 80); // Positioning of the text element
+      
+      Debugger.log("Generated text for: #canvas-one");
       
     }
     
@@ -48,8 +53,10 @@
   }
   
   function eventWindowLoaded() {
+    Debugger.log("Loaded page");
     canvasApp();
   }
-
+  
   window.addEventListener("load", eventWindowLoaded, false);
+  
 }());
